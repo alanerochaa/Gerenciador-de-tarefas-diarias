e5b0d270e279f54f5f401fd842490d981170688c
Alane Rocha <alanerochaa@gmail.com>
2025-03-06 17:52:03 -0300

    Selecionando elementos do DOM

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 let tarefas = [
     { id: 1, titulo: "Finalizar o projeto", status: "pendente" }
 ];
+
+// 2️⃣ Selecionar elementos do DOM
+const inputTarefa = document.getElementById("tarefa-input");
+const botaoAdicionar = document.getElementById("adicionar-btn");
+const botaoFiltrar = document.getElementById("filtrar-btn");
+const listaPendentes = document.getElementById("pendentes-lista");
+const listaEmAndamento = document.getElementById("em-andamento-lista");
+const listaConcluidas = document.getElementById("concluidas-lista");
+
